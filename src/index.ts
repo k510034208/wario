b7723efdea73d2d7351a076868e3dfcc3b039c92
k510034208 <k510034208.n@gmail.com>
2021-05-08 21:03:52 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,6 @@ createConnection().then(async () => {
     // start express server
     app.listen(3000);
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+    console.log("Express server has started on port 3000.");
 
 }).catch(error => console.log(error));
