df69e9f7583c0396d1f879364349c1299710f960
k510034208 <k510034208.n@gmail.com>
2021-05-09 00:27:57 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import {createConnection} from "typeorm";
 import {createExpressServer} from 'routing-controllers'
 import {EventController} from './controller/EventControllers'
 import {WebhookController} from './controller/WebhookControllers'
+import {middleware, MiddlewareConfig} from "@line/bot-sdk";
+
+const middlewareConfig: MiddlewareConfig = {
+    channelAccessToken: 'xuY+Bdfl62TcU0Bvtoi/FMMrQaZLzIjIFDPs4K0WXeK/fcnpTvYTr59No0MRxaI3WQWMrRtRtx5SUScEMP6l6/XQQjUGV6SnsXJ6/6OgJq9PPaGfnZS7uTTKN6Yi0eU3Cr6BIHI4EdDi/IYgvw1pEgdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '719db9afad629abe7858c8ad439498f3',
+}
 
 createConnection().then(async () => {
 
@@ -11,12 +17,15 @@ createConnection().then(async () => {
         controllers: [
             EventController,
             WebhookController
+        ],
+        middlewares: [
+            middleware(middlewareConfig)
         ]
     })
 
     // start express server
-    app.listen(process.env.PORT);
+    app.listen(3000);
 
-    console.log("Express server has started on port 3000.");
+    console.log(`Express server has started on port 3000.`);
 
 }).catch(error => console.log(error));
